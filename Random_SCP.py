ce444c52e8cb6105204bad2536a68cf64cb55b15
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2019-07-15 11:25:44 +0530

    Added docstring xD

--- Random_SCP.py
+++ Random_SCP.py
@@ -1,6 +1,14 @@
 import webbrowser
 import random
 
+
+''' Can open any scp article in the range 1 - 4999
+    Just click on the file.
+    It will run the code in the background
+    and open a new tab with a random scp article in your default browser.
+'''
+
+
 x = random.randint(1, 5000)
 if x < 10:
     url = f"http://www.scp-wiki.net/scp-00{str(x)}"
@@ -10,5 +18,6 @@ elif x >= 100 and x < 1000:
     url = f"http://www.scp-wiki.net/scp-{str(x)}"
 elif x >= 1000:
     url = f"http://www.scp-wiki.net/scp-{str(x)}"
+
 
 webbrowser.open_new_tab(url)
