5d99d96998ce03aaf4b5c318322b294c91c22f13
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-03-19 14:05:36 +0530

    Update Random_SCP.py

--- Random_SCP.py
+++ Random_SCP.py
@@ -7,17 +7,6 @@ import random
     It will run the code in the background
     and open a new tab with a random scp article in your default browser.
 '''
-
-
-x = random.randint(1, 5000)
-if x < 10:
-    url = f"http://www.scp-wiki.net/scp-00{str(x)}"
-elif x >= 10 and x < 100:
-    url = f"http://www.scp-wiki.net/scp-0{str(x)}"
-elif x >= 100 and x < 1000:
-    url = f"http://www.scp-wiki.net/scp-{str(x)}"
-elif x >= 1000:
-    url = f"http://www.scp-wiki.net/scp-{str(x)}"
-
+url = http://www.scp-wiki.net/random:random-scp
 
 webbrowser.open_new_tab(url)
